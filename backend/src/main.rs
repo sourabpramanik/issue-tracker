2c727ec4b1eb97169782da177a54aeb964358140
sourabpramanik <shubpramanik241@gmail.com>
2024-02-01 21:13:50 +0530

    feat: added get all tasks handler

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -6,13 +6,44 @@ use actix_web::{
 use clerk_rs::{
     apis::users_api::User, clerk::Clerk, validators::actix::ClerkMiddleware, ClerkConfiguration,
 };
+use serde::{Deserialize, Serialize};
 use shuttle_actix_web::ShuttleActixWeb;
+use shuttle_runtime::CustomError;
 use shuttle_secrets::SecretStore;
-use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+use sqlx::{Executor, FromRow, PgPool};
 
 struct AppState {
     client: Clerk,
-    pool: Pool<Postgres>,
+    pool: PgPool,
+}
+
+#[derive(Serialize, Deserialize, FromRow)]
+struct Task {
+    id: String,
+    title: String,
+    description: String,
+    status: String,
+    lable: String,
+    author: String,
+}
+
+#[get("/tasks")]
+async fn get_tasks(state: web::Data<AppState>) -> impl Responder {
+    let query: Result<Vec<Task>, sqlx::Error> = sqlx::query_as("SELECT * FROM Task")
+        .fetch_all(&state.pool)
+        .await;
+
+    let tasks = match query {
+        Ok(value) => value,
+        Err(e) => {
+            return HttpResponse::InternalServerError().json(serde_json::json!({
+                "status": "FAILED",
+                "message": e.to_string(),
+            }));
+        }
+    };
+
+    HttpResponse::Ok().json(tasks)
 }
 
 #[get("/users")]
@@ -47,23 +78,12 @@ async fn get_users(state: web::Data<AppState>, _req: HttpRequest) -> impl Respon
 #[shuttle_runtime::main]
 async fn actix_web(
     #[shuttle_secrets::Secrets] secrets: SecretStore,
-    #[shuttle_shared_db::Postgres] db_conn_str: String,
+    #[shuttle_shared_db::Postgres] pool: PgPool,
 ) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     // DB Pool
-    let pool = match PgPoolOptions::new()
-        .max_connections(10)
-        .connect(&db_conn_str)
+    pool.execute(include_str!("../schema.sql"))
         .await
-    {
-        Ok(pool) => {
-            println!("🚀 Connection to the database is successful!");
-            pool
-        }
-        Err(err) => {
-            println!("🔥 Failed to connect to the database: {:?}", err);
-            std::process::exit(1);
-        }
-    };
+        .map_err(CustomError::new)?;
 
     // Clerk integration
     let clerk_secret_key = secrets
@@ -79,7 +99,8 @@ async fn actix_web(
         cfg.service(
             web::scope("/api")
                 .wrap(ClerkMiddleware::new(clerk_config, None, true))
-                .service(get_users),
+                .service(get_users)
+                .service(get_tasks),
         )
         .service(actix_files::Files::new("/", "./frontend/dist").index_file("index.html"))
         .app_data(state);
