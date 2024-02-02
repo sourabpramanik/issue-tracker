4024f9e2c00d06d742a2b814041e59ca3b61c163
sourabpramanik <shubpramanik241@gmail.com>
2024-02-02 17:30:24 +0530

    feat: toaster

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { ClerkProvider } from '@clerk/clerk-react'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ClerkProvider } from "@clerk/clerk-react";
+import { Toaster } from "@/components/ui/sonner";
 
 // Import your publishable key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
+  throw new Error("Missing Publishable Key");
 }
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <App />
+      <Toaster />
     </ClerkProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
